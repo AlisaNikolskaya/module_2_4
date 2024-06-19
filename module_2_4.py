bc654a29eb026ee10e92a625a962f38fa91a3969
AlisaNikolskaya <nikolalisa13@gmail.com>
2024-06-19 15:49:59 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -2,19 +2,19 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for number in numbers:
-    if number < 2:
-        not_primes.append(number)
+for num in numbers:
+    if num < 2:
+        continue
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(num)
     else:
-        is_prime = True
-        for i in range(2, number):
-            if number % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(number)
-        else:
-            not_primes.append(number)
+        not_primes.append(num)
 
 print("Список простых чисел:", primes)
 print("Список составных чисел:", not_primes)
